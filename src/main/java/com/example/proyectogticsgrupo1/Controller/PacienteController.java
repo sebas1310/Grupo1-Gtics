7b39f761f37d6c59b5e9a7dfa5125fb3e1ee00fb
sebas1310 <a20203368@pucp.edu.pe>
2023-04-08 13:55:34 -0500

    paciente dashboard

--- src/main/java/com/example/proyectogticsgrupo1/Controller/PacienteController.java
+++ src/main/java/com/example/proyectogticsgrupo1/Controller/PacienteController.java
@@ -1,7 +1,14 @@
 package com.example.proyectogticsgrupo1.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/paciente")
 public class PacienteController {
-public String prueb(){
-return "r";
-}
+    @GetMapping(value = "/")
+    public String paciente(){
+        return "paciente/index";
+    }
 }
