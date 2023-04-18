18cb998b1f3c78a06698d9ef9ff3330224fa84a4
angie2512 <a20191792@pucp.edu.pe>
2023-04-18 01:14:00 -0500

    controllersadministrador

--- src/main/java/com/example/proyectogticsgrupo1/Controller/AdministradorController.java
+++ src/main/java/com/example/proyectogticsgrupo1/Controller/AdministradorController.java
@@ -8,10 +8,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/administrador")
 
 public class AdministradorController {
-        @GetMapping(value = "/")
-        public String paciente() {
-            return "administrador/dashboard";
+    @GetMapping(value = "/")
+    public String administrador() {
+        return "administrador/dashboard";
         }
+
+    @GetMapping(value = "/perfilAdministrador")
+    public String perfilAdministrador() {
+
+        return "administrador/perfil";
+    }
+    @GetMapping(value = "/nuevoPaciente")
+    public String newPaciente() {
+
+        return "administrador/nuevopaciente";
+    }
+
+    @GetMapping(value = "/generalCalendario")
+    public String genCalendar() {
+
+        return "administrador/calendariogeneral";
+    }
+
+    @GetMapping(value = "/formatos")
+    public String formatos() {
+
+        return "administrador/formatos";
+    }
+
+    @GetMapping(value = "/configuraciones")
+    public String config() {
+
+        return "administrador/configuraciones";
+    }
+
+
+
+
 
 
     }
