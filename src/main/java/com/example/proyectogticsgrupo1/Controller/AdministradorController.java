b00c2bfdd5e29d21f32a9d55449569ee85e5ef73
angie2512 <a20191792@pucp.edu.pe>
2023-04-18 17:52:24 -0500

    cambiosfinales

--- src/main/java/com/example/proyectogticsgrupo1/Controller/AdministradorController.java
+++ src/main/java/com/example/proyectogticsgrupo1/Controller/AdministradorController.java
@@ -3,27 +3,28 @@ package com.example.proyectogticsgrupo1.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value="/administrador", method = RequestMethod.GET)
 
 public class AdministradorController {
-    @GetMapping("/administrador")
+    @GetMapping("")
     public String administrador() {
         return "administrador/dashboard";
         }
 
-    @GetMapping(value = "/perfilAdministrador")
+    @GetMapping(value = "/perfil")
     public String perfilAdministrador() {
 
         return "administrador/perfil";
     }
     @GetMapping(value = "/nuevopaciente")
     public String newPaciente() {
-
         return "administrador/nuevopaciente";
     }
 
-    @GetMapping(value = "/generalCalendario")
+    @GetMapping(value = "/calendariogeneral")
     public String genCalendar() {
 
         return "administrador/calendariogeneral";
@@ -39,6 +40,66 @@ public class AdministradorController {
     public String config() {
 
         return "administrador/configuraciones";
+    }
+
+    @GetMapping(value = "/dashboardpaciente")
+    public String dashboardpacient() {
+
+        return "administrador/dashboardpaciente";
+    }
+
+    @GetMapping(value = "/dashboarddoctor")
+    public String dashboarddoc() {
+
+        return "administrador/dashboarddoctor";
+    }
+
+    @GetMapping(value = "/dashboardfinanzas")
+    public String dashboardfinanz() {
+
+        return "administrador/dashboardfinanzas";
+    }
+
+    @GetMapping(value = "/historialclinico")
+    public String historialClinico() {
+
+        return "administrador/historialclinico";
+    }
+
+    @GetMapping(value = "/crearpaciente")
+    public String crearPaciente() {
+
+        return "administrador/crearpaciente";
+    }
+
+    @GetMapping(value = "/invitar")
+    public String invitar() {
+
+        return "administrador/invitar";
+    }
+
+    @GetMapping(value = "/vistaformato")
+    public String vistaForm() {
+
+        return "administrador/vistaformato";
+    }
+
+    @GetMapping(value = "/notificaciones")
+    public String notif() {
+
+        return "administrador/notificaciones";
+    }
+
+    @GetMapping(value = "/mensajes")
+    public String mensajes() {
+
+        return "administrador/mensajes";
+    }
+
+    @GetMapping(value = "/chat")
+    public String chat() {
+
+        return "administrador/chat";
     }
 
 
