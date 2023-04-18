c6a024876021b8ba36d03d977fb7476419865e9f
sebas1310 <a20203368@pucp.edu.pe>
2023-04-18 14:52:47 -0500

    administrativo fin

--- src/main/java/com/example/proyectogticsgrupo1/Repository/PacienteRepository.java
+++ src/main/java/com/example/proyectogticsgrupo1/Repository/PacienteRepository.java
@@ -1,7 +1,8 @@
 package com.example.proyectogticsgrupo1.Repository;
 
 import com.example.proyectogticsgrupo1.Entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
 public interface PacienteRepository extends JpaRepository<Paciente,Integer> {
 }
+*/
