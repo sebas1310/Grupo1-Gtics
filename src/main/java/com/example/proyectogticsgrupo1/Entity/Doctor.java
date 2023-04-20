843f8a975c74c2f5d38b0fca4d91ea8dd641ecef
sebas1310 <a20203368@pucp.edu.pe>
2023-04-20 17:59:26 -0500

    entity fin

---
+++ src/main/java/com/example/proyectogticsgrupo1/Entity/Doctor.java
@@ -0,0 +1,41 @@
+package com.example.proyectogticsgrupo1.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name ="doctor")
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "iddoctor",  nullable = false)
+    private Integer iddcotor;
+
+    @ManyToOne
+    @JoinColumn(name = "idespecialidad",  nullable = false)
+    private Especialidad especialidad;
+
+    @ManyToOne
+    @JoinColumn(name = "idsede",  nullable = false)
+    private Sede sede;
+
+    @Column(name = "cmp",  nullable = false)
+    private Integer cmp;
+
+    @Column(name = "formacion",  nullable = false)
+    private String formacion;
+
+    @Column(name = "me",  nullable = false)
+    private Integer me;
+
+    @Column(name = "capacitaciones",  nullable = false)
+    private String capacitaciones;
+
+    @ManyToOne
+    @JoinColumn(name = "idusuario",  nullable = false)
+    private Usuario usuario;
+
+}
