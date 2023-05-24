425a27908d6c1e0769a2fccc661e6f00a260ffb4
adrianlopez981 <adrian.lopez@pucp.edu.pe>
2023-05-23 19:15:26 -0500

    "merge de spa"

--- src/main/java/com/example/proyectogticsgrupo1/Entity/DatosEntity.java
+++ src/main/java/com/example/proyectogticsgrupo1/Entity/DatosEntity.java
@@ -3,7 +3,7 @@ package com.example.proyectogticsgrupo1.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-/*
+
 
 @Getter
 @Setter
@@ -30,4 +30,3 @@ public class DatosEntity {
 
 
 }
-*/
