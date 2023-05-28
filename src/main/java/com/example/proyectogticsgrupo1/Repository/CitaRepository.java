e926d1a38709271d285d9d3cc5c0bfd631508887
Abigail Ramirez <abigail.ramirez@pucp.edu.pe>
2023-05-27 22:06:11 -0500

    cambio doctor

--- src/main/java/com/example/proyectogticsgrupo1/Repository/CitaRepository.java
+++ src/main/java/com/example/proyectogticsgrupo1/Repository/CitaRepository.java
@@ -37,7 +37,7 @@ public interface CitaRepository extends JpaRepository<Cita, Integer> {
     List<Cita> findByPacienteAndFechaAfterOrderByFechaAsc(Paciente paciente, LocalDate fechaactual);
 
     @Query(value= "select * from cita where doctor_iddoctor= ?1 and concat(fecha, ' ' , horainicio , ' ', horafinal) <=current_timestamp()\n" +
-            "order by concat(fecha, ' ' , horainicio ) DESC",nativeQuery = true)
+            "group by paciente_idpaciente",nativeQuery = true)
     List<Cita> pacientesAtendidosPorDoctor(Integer idDoctor);
 
     @Query(value= "select * from cita where paciente_idpaciente= ?1 and doctor_iddoctor = ?2 ",nativeQuery = true)
