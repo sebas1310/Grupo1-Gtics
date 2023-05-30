d3dc358f119d873fe55914eb6c70de2537e60c48
adrianlopez981 <adrian.lopez@pucp.edu.pe>
2023-05-30 16:03:29 -0500

    "entidades de tablas plantillas para listado"

---
+++ src/main/java/com/example/proyectogticsgrupo1/Entity/DatosJsonEntity.java
@@ -0,0 +1,31 @@
+package com.example.proyectogticsgrupo1.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "datos_json", schema = "bdclinicag1", catalog = "")
+public class DatosJsonEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nombre_plantilla",  nullable = false)
+    private String nombre_plantilla;
+
+    @Column(name = "datos_llenos")
+    private Object datosLlenos;
+
+    @ManyToOne
+    @JoinColumn(name = "idusuario",  nullable = false)
+    private Usuario usuario;
+
+    @Column(name = "modelo_json_id", nullable = false)
+    private int modelo_json_id;
+
+    @ManyToOne
+    @JoinColumn(name = "cita_idcita")
+    private Cita cita;
+
+
+}
