cb6cf1b7a4f72036b6912cfda6b93fc003ccf342
angie2512 <a20191792@pucp.edu.pe>
2023-06-05 23:30:41 -0500

    olvidocontrasena, cambiarcontrasena

--- src/main/java/com/example/proyectogticsgrupo1/Controller/LoginController.java
+++ src/main/java/com/example/proyectogticsgrupo1/Controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.example.proyectogticsgrupo1.Controller;
 
-import com.example.proyectogticsgrupo1.Entity.Usuario;
+import com.example.proyectogticsgrupo1.Entity.*;
 import com.example.proyectogticsgrupo1.Repository.UsuarioRepository;
+import com.example.proyectogticsgrupo1.Service.EmailService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,10 +20,15 @@ public class LoginController {
 
     final UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private EmailService emailService;
+
 
     public LoginController(UsuarioRepository usuarioRepository){
         this.usuarioRepository = usuarioRepository;
     }
+
+
 
     @GetMapping(value = {"/"})
     public String inicioSesion(){
@@ -74,9 +81,21 @@ public class LoginController {
     }
 
     @GetMapping(value = "/cambiarcontrasena")
-    public String olvidoContra(){
+    public String cambiarContra(){
 
         return "cambiarcontrasena";
+    }
+
+    @GetMapping(value = "/cambiarcontrasena2")
+    public String cambiarContra2(){
+
+        return "cambiarcontrasena2";
+    }
+
+    @GetMapping(value = "/olvidocontra")
+    public String olvidoContra(){
+
+        return "/superadmin/olvidocontra_spa";
     }
 
     @GetMapping(value = "/formRegistro")
@@ -110,5 +129,48 @@ public class LoginController {
 
         return "redirect:/cambiarcontrasena";
     }
+
+    @PostMapping(value = "/changepassword2")
+    @Transactional
+    public String changePassword2(@RequestParam("correo") String email, @RequestParam("newpassword") String newpassword, @RequestParam("renewpassword") String renewpassword, RedirectAttributes redirectAttributes) {
+
+        // Obtener el usuario por su correo electrónico desde la base de datos
+        Usuario usuario = usuarioRepository.findByCorreo(email);
+
+        if (usuario != null) {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                String hashedNewPassword = passwordEncoder.encode(newpassword);
+
+                // Actualizar la contraseña en la base de datos
+                usuarioRepository.changePassword(hashedNewPassword, usuario.getIdusuario());
+                redirectAttributes.addFlashAttribute("psw1", "Contraseña actualizada, prueba iniciando sesión");
+
+        } else {
+            redirectAttributes.addFlashAttribute("psw3", "No se pudo actualizar, no se encontró ningún usuario con el correo electrónico proporcionado");
+        }
+
+        return "redirect:/cambiarcontrasena2";
+    }
+
+    @PostMapping(value = "/enviarcorreo")
+    public String enviarCorreo(@RequestParam("dni") String dni, @RequestParam("correo") String correo, RedirectAttributes redirectAttributes) {
+        // Verificar si existe un correo en la base de datos
+        Usuario usuario = usuarioRepository.findByCorreo(correo);
+
+        if (usuario != null && usuario.getDni().equals(dni)) {
+            // Lógica para enviar el correo electrónico
+            emailService.sendEmail(correo, "Cambio de Contraseña","Estimado usuario, usted ha solicitado un cambio de contraseña:\nIngresa aquí para cambiarla: http://localhost:8081/cambiarcontrasena2");
+            redirectAttributes.addFlashAttribute("ms1", "El correo ha sido enviado exitosamente");
+        } else if (usuario == null) {
+            redirectAttributes.addFlashAttribute("ms2", "No se pudo enviar el correo, no se encontró ningún usuario con el correo electrónico proporcionado");
+        } else {
+            redirectAttributes.addFlashAttribute("ms3", "El correo y DNI no coinciden");
+        }
+
+        return "redirect:/olvidocontra";
+    }
+
+
+
 
 }
