1d480b5f9955d1b0f6ecbaaedee31b9e998c17b3
angie2512 <a20191792@pucp.edu.pe>
2023-06-06 15:56:13 -0500

    error

--- src/main/java/com/example/proyectogticsgrupo1/ProyectoGticsGrupo1Application.java
+++ src/main/java/com/example/proyectogticsgrupo1/ProyectoGticsGrupo1Application.java
@@ -2,6 +2,8 @@ package com.example.proyectogticsgrupo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @SpringBootApplication
 public class ProyectoGticsGrupo1Application {
@@ -9,6 +11,15 @@ public class ProyectoGticsGrupo1Application {
     public static void main(String[] args) {
 
         SpringApplication.run(ProyectoGticsGrupo1Application.class, args);
+    }
+
+    @ControllerAdvice
+    public class GlobalExceptionHandler {
+
+        @ExceptionHandler(Exception.class)
+        public String error() {
+            return "error"; // Nombre de la vista de error, por ejemplo, "error.html"
+        }
     }
 
 }
