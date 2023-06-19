ea2cb93e0ab945e35f7703058bc57190642fdd85
angie2512 <a20191792@pucp.edu.pe>
2023-06-19 01:52:48 -0500

    finanzas completo

---
+++ src/main/java/com/example/proyectogticsgrupo1/DTO/IngresosEgresos.java
@@ -0,0 +1,10 @@
+package com.example.proyectogticsgrupo1.DTO;
+
+public interface IngresosEgresos {
+
+    float getpromedio_monto_doctor();
+    float getpromedio_monto_paciente();
+    String getnombre_mes();
+
+
+}
