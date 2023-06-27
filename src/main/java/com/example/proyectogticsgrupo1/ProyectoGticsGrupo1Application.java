bf37905fe0ac7fc98dcf594685ba0d45429435f9
angie2512 <a20191792@pucp.edu.pe>
2023-06-27 02:50:58 -0500

    calendario arreglos

--- src/main/java/com/example/proyectogticsgrupo1/ProyectoGticsGrupo1Application.java
+++ src/main/java/com/example/proyectogticsgrupo1/ProyectoGticsGrupo1Application.java
@@ -1,9 +1,19 @@
 package com.example.proyectogticsgrupo1;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class ProyectoGticsGrupo1Application {
@@ -13,13 +23,29 @@ public class ProyectoGticsGrupo1Application {
         SpringApplication.run(ProyectoGticsGrupo1Application.class, args);
     }
 
-    @ControllerAdvice
-    public class GlobalExceptionHandler {
+    @Configuration
+    public class ErrorConfig {
 
-        @ExceptionHandler(Exception.class)
-        public String error() {
-            return "error"; // Nombre de la vista de error, por ejemplo, "error.html"
+        @Bean
+        public ErrorViewResolver customErrorViewResolver() {
+            return (HttpServletRequest request, HttpStatus status, Map<String, Object> model) -> {
+                ModelAndView modelAndView = new ModelAndView("error"); // Nombre de tu página de error personalizada
+                modelAndView.addObject("status", status.value());
+                modelAndView.addObject("error", status.getReasonPhrase());
+                return modelAndView;
+            };
         }
     }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/assets/**")
+                    .addResourceLocations("classpath:/static/assets/");
+        }
+    }
+
 
 }
