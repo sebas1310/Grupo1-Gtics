36f1489c690c61bb327dce73274e1f30095a8d8c
alexia2212 <a20192270@pucp.edu.pe>
2023-07-15 11:43:43 -0500

    guardar usuarios

--- src/main/resources/static/validar.js
+++ src/main/resources/static/validar.js
@@ -8,13 +8,19 @@ $(document).ready(()=>{
 function validarContrasena(){
     const pass1 = $('#input1').val();
     const pass2 = $('#input2').val();
+    const nombre = $('#nombres').val();
+    const apellido = $('#apellidos').val();
+    const dni = $('#dni').val();
+    const celular = $('#celular').val();
+    const direccion = $('#direccion').val();
+
     if(pass1==pass2){
         $('#button_submit').removeClass('disabled')
     }else{
         $('#button_submit').removeClass('disabled')
         $('#button_submit').addClass('disabled')
 
-    }if(pass1.trim() === "" || pass2.trim() === ""){
+    }if(pass1.trim() === "" || pass2.trim() === "" || nombre.trim() === "" || apellido.trim() === "" || dni.trim() === "" || celular.trim() === "" || direccion.trim() === ""){
         var errorMessage = document.getElementById("error-message");
         errorMessage.style.display = "block";
         errorMessage.innerHTML = "No se permiten espacios en blanco.";
